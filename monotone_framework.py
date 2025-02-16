d28f03bc9e546a769970d9008e96fc050dd53b94
PetermasterPerfect <petermasterperfect@gmail.com>
2025-02-16 21:03:50 +0100

    MDFAF class at the end of the day should take analysis object which sets proper constraints variables(specifically it sets function depending on cfg node type).

--- monotone_framework.py
+++ monotone_framework.py
@@ -11,50 +11,58 @@ def cfg_dfs(node, tab=set()):
         tab|=cfg_dfs(s, tab)
     return tab 
 
-def exp_f(constraints, node):
-    ret = set()
-    for s in node.successors:
-        ret |= constraints[s]
-    return ret | node.ast_node.get_used_vars()
+class LiveVariablesAnalysis:
+    def __init__(self, cfg):
+        self.lattice_bottom = set()
+        self.bound = cfg.exit_stm
 
-def assignment_f(constraints, node):
-    ret = set()
-    for s in node.successors:
-        ret |= constraints[s]
-    return ret - {node.ast_node.name} | node.ast_node.exp.get_used_vars()
 
-def return_f(constraints, node):
-    return node.ast_node.get_used_vars()
+    def exp_f(constraints, node):
+        ret = set()
+        for s in node.successors:
+            ret |= constraints[s]
+        return ret | node.ast_node.get_used_vars()
 
-def show(sol):
-    print(dict(zip([x.ast_node for x in sol], [sol[x] for x in sol])))
+
+    def assignment_f(constraints, node):
+        ret = set()
+        for s in node.successors:
+            ret |= constraints[s]
+        return ret - {node.ast_node.name} | node.ast_node.exp.get_used_vars()
+
+
+    def return_f(constraints, node):
+        return node.ast_node.get_used_vars()
+
+
+    def constraints_from_cfg(self, nodes):
+        fun_constraints = dict()
+        for node in nodes:
+            print(node.ast_node)
+            if node == self.bound:
+                fun_constraints[node] = LiveVariablesAnalysis.return_f
+            elif type(node.ast_node) is AssignAstNode:
+                fun_constraints[node] = LiveVariablesAnalysis.assignment_f
+            else:
+                fun_constraints[node] = LiveVariablesAnalysis.exp_f
+        return fun_constraints
+
 
 
 class MDFAF: # monotone data flow analysis framework
-    def __init__(self, graph):
+    def __init__(self, graph, model_analysis):
         self.function_vars = graph.ast_node.vars
         self.cfg = graph
         self.nodes = list(cfg_dfs(graph.start_stm))
-        self.node_to_constraints()
+        self.model_analysis = model_analysis
+        self.fun_constraints = self.model_analysis.constraints_from_cfg(self.nodes)
 
 
     def solve(self):
-        sol = dict(zip(self.nodes, [set() for x in self.fun_constraints]))
+        sol = dict(zip(self.nodes, [self.model_analysis.lattice_bottom for x in self.fun_constraints]))
         f = [ self.fun_constraints[e](sol, self.nodes[i]) for i, e in enumerate(self.fun_constraints) ]
         while sol != f:
             for i, e in enumerate(self.fun_constraints):
                 sol[e] = self.fun_constraints[e](sol, e)
             f = dict(zip(self.nodes, [ self.fun_constraints[e](sol, self.nodes[i]) for i, e in enumerate(self.fun_constraints) ]))
         return sol
-    
-
-    def node_to_constraints(self):
-        self.fun_constraints = dict()
-        for node in self.nodes:
-            print(node.ast_node)
-            if node == self.cfg.exit_stm:
-                self.fun_constraints[node] = return_f
-            elif type(node.ast_node) is AssignAstNode:
-                self.fun_constraints[node] = assignment_f
-            else:
-                self.fun_constraints[node] = exp_f
